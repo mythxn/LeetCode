3901d8534e7ee0c569415a8d441518303cd32cd5
mythxn <mithain3@icloud.com>
2021-06-14 17:01:00 +0400

    14/06: daily update

---
+++ 34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=34 lang=python3
+#
+# [34] Find First and Last Position of Element in Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = self.binSearch(nums, target, "left")
+        right = self.binSearch(nums, target, "right")
+
+        return [left, right]
+
+    # if leftBias false; res is rightBias
+    def binSearch(self, nums, target, bias):
+        l, r = 0, len(nums) - 1
+        i = -1
+
+        while l <= r:
+            m = (l + r) // 2
+            if target > nums[m]:
+                l = m + 1
+            elif target < nums[m]:
+                r = m - 1
+            else:
+                i = m
+                if bias == "left":
+                    r = m - 1
+                elif bias == "right":
+                    l = m + 1
+        return i
+
+
+# @lc code=end
