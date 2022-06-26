7a97f0c8ac7001381c78a9ff7c96c74267344dbd
mythxn <mithun.shanoj@dubizzle.com>
2022-06-26 15:30:57 +0400

    top-k-frequent-elements

---
+++ top-k-frequent-elements.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+        return sorted(freq, key=freq.get, reverse=True)[:k]
