5fed577cb5ec160d5801e555b3b885b84786f5f5
mythxn <mithun.shanoj@dubizzle.com>
2022-09-04 08:46:09 +0400

    binary search

---
+++ binary-search.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # Binary search
+        # Time complexity: O(log n)
+        # Space complexity: O(1)
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
